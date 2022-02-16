98e77660ab1d1360684d6994086382aac410ae56
Kevin <kevin03.nguyen@gmail.com>
2022-02-16 17:34:27 -0500

    add cart card template

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 const CartCard = () => {
   return (
-    <div className="cart-card">
-      <div>Chicken</div>
-      <div>edit</div>
-    </div>
+    <Row className="cart-card">
+      <Col className="cart-card__left" xs="12" sm="12" md="8" lg="7">
+        <Row className="h-100 border-box">
+          <Col xs="3" sm="3" md="5" lg="4" xl="3" xxl="3">
+            <img
+              className="cart-icon"
+              src="https://img-c.udemycdn.com/course/240x135/1672410_9ff1_5.jpg"
+            />
+          </Col>
+          <Col>
+            <div>Chicken Fingers</div>
+            <div>Subtotal</div>
+          </Col>
+        </Row>
+      </Col>
+      <Col className="cart-card__right">
+        <div className="cart-card__btn">
+          <input className="input-quantity" type="number" value="1"></input>
+          <button>Update</button>
+          <button>Remove</button>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
--- src/components/CartDisplay.js
+++ src/components/CartDisplay.js
@@ -4,16 +4,17 @@ const CartDisplay = () => {
   return (
     <div className="cart-display h-100  display-flex flex-wrap">
       <div className="cart-display__right display-flex">
-        <div className="subtotal">
-          Subtotal: <span className="price">price</span>
+        <div className="total">
+          Total: <span className="price">price</span>
         </div>
-        <div>
+        <div className="h-100 w-100">
           <button className="checkout">Checkout</button>
         </div>
       </div>
 
       <div className="cart-display__left display-flex">
         <div className="course-cart">2 Courses in Cart</div>
+        <CartCard />
         <CartCard />
       </div>
     </div>
